d4acc7703265bf6da5b7ec6089038f33e54240ee
Maximos Salzman <maximossalzman@gmail.com>
2021-03-01 13:26:49 -0500

    changes styling for buttons and inputs

--- src/components/CreateTaskScreen.js
+++ src/components/CreateTaskScreen.js
@@ -22,13 +22,13 @@ export default function CreateTaskScreen({navigation}) {
     return (
         <View style={styles.container}>
             <ImageBackground style={styles.image} source={require("../../assets/background-image.jpg")} />
-         <ScrollView >
+         {/* <ScrollView > */}
 
-             <View style={styles.scrollContainer}>
+             {/* <View style={styles.scrollContainer}> */}
                  <View style={styles.formContainer}>
                     <TextInput style={styles.input} placeholder="task name" onChangeText={(text) => {setTaskName(text)}}/>
                     <RNPickerSelect 
-                        style={styles.picker}
+                        style={pickerSelectStyles}
                         onValueChange={value => setTaskType(value)}
                         items={[
                             {label: "Daily", value: "Daily"},
@@ -38,8 +38,8 @@ export default function CreateTaskScreen({navigation}) {
                         />
                     <Button title="submit" onPress={handleSubmit}/>
                  </View>
-             </View>
-        </ScrollView>
+             {/* </View> */}
+        {/* </ScrollView> */}
         </View>
     )
 }
@@ -51,18 +51,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
     scrollContainer: {
+    width: "100%",
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 50,
   },
   input: {
+    backgroundColor: "dodgerblue",
     margin: 20,
-    fontSize: 30
-  },
-  picker: {
-    textAlign: "center",
     fontSize: 30,
+    borderRadius: 5,
+    padding: 10,
+    color: "white",
   },
   image: {
     position: "absolute",
@@ -71,9 +72,35 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
   formContainer: {
+    padding: 20,
       width: "90%",
       height: "100%",
       borderRadius: 5,
       backgroundColor: "rgba(255,255,255, 0.5)"
   }
 })
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    margin: 20,
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    backgroundColor: "dodgerblue",
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'white',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { View, Text , StyleSheet, Button} from 'react-native'
+import { TouchableOpacity, View, Text , StyleSheet, Button} from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack';
 import ProductDetailScreen from './ProductDetailScreen';
 import {useDispatch, useSelector} from 'react-redux'
@@ -54,8 +54,18 @@ export default function ProductDetail({product,navigation}) {
 
         <View style={completed ? styles.completed : styles.item}>
             {detailRevealed && <ProductDetailScreen setDetailRevealed={setDetailRevealed} task={task}/>}
-            <Button title={task.title} onPress={productReroute}/>
-            <Button title={task.completed? "mark not completed" : "mark completed" } onPress={taskComplete}/>
+             <TouchableOpacity
+                    style={styles.button}
+                    onPress={productReroute}
+            >
+                    <Text style={styles.buttonText}>{task.title}</Text>
+            </TouchableOpacity>
+             <TouchableOpacity
+                    style={styles.button}
+                    onPress={taskComplete}
+            >
+                    <Text>{task.completed? "mark not completed" : "mark completed" }</Text>
+            </TouchableOpacity>
             {/* { product && product.data.map((item, idx)=> <Text key={idx}>{item}</Text>)} */}
         </View>
     )
@@ -63,15 +73,25 @@ export default function ProductDetail({product,navigation}) {
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: "goldenrod",
+        backgroundColor: "red",
         padding: 20,
+        borderRadius: 10,
         marginVertical: 8,
         marginHorizontal: 20,
     },
     completed: {
-        backgroundColor: "yellow",
+        backgroundColor: "green",
         padding: 20,
+        borderRadius: 10,
         marginVertical: 8,
         marginHorizontal: 20,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#00eaFF',
+        padding: 10
+    },
+    buttonText: {
+        fontSize: 30,
     }
 })
